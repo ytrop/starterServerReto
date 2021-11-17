c9b2d2aeb71866274075707a48a1be97a6ef9a6d
ytrop <62332814+ytrop@users.noreply.github.com>
2021-11-17 17:04:15 +0100

    Add files via upload

---
+++ starterServerReto/src/main/java/com/nttdata/bootcamp/starterServerReto/StarterServerRetoApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.starterServerReto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterServerRetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterServerRetoApplication.class, args);
+	}
+
+}
---
+++ starterServerReto/src/test/java/com/nttdata/bootcamp/starterServerReto/StarterServerRetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.starterServerReto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterServerRetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
